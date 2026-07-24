1e73490985a5b835d34f7c210ceb37f804df0c44
felipehtt <fs.oliveira0014@gmail.com>
2026-07-24 15:39:42 -0300

    import's

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import './utils/global.js'
+
+import 'dotenv/config.js';
+import express from 'express';
+import cors from 'cors';
+
+
+const servidor = express();
+servidor.use(cors());
+servidor.use(express.json());
+
+
+const PORTA = process.env.PORTA;
+servidor.listen(PORTA, () => console.log(`APi subiu na ${PORTA}`)) 
